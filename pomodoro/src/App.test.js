962e0fcc808b92050e059f387e4ac41776729a87
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2020-04-24 12:53:48 +0000

    Changed to title of Timer at App.test.js

--- pomodoro/src/App.test.js
+++ pomodoro/src/App.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import App from './App';
+import Timer from './App';
 
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
+  const { getByText } = render(<Timer />);
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
