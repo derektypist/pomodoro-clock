39ccb1fccd0b22eae77db99b38fe487e5525f05f
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2020-04-29 16:24:15 +0000

    Updates

---
+++ precache-manifest.5aef8c4203c603acdc8a34bef2afc43b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a34fba66e2cc932f10007490be82152",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d5b214e4265a1e64239e",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "0a88b30c2e0784641c54",
+    "url": "/static/js/2.30078677.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.30078677.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5b214e4265a1e64239e",
+    "url": "/static/js/main.521e5dc7.chunk.js"
+  },
+  {
+    "revision": "0393184234ba9b4731d9",
+    "url": "/static/js/runtime-main.e6333730.js"
+  }
+]);
