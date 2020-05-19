d557b767e788113a656e66289347631b0ee8cb3a
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2020-05-19 15:41:21 +0000

    Updated timer controls at App.js

--- pomodoro/src/App.js
+++ pomodoro/src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
 
-let accurateInterval = require('accurate-interval');
-
 // Set Up Components
 class TimerLengthControl extends React.Component {
     render() {
@@ -29,132 +27,60 @@ class TimerLengthControl extends React.Component {
 };
 
 class Timer extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
         mode: 'session',
         active: false,
         brkLength: 5,
         seshLength: 25,
-        timer: 1500     
+        timer: 1500,
+        alarmColor: {color: 'white'}     
     };
    
   }
     
+  // Updates state when session or break ends and plays sound
     componentDidUpdate() {
     if(this.state.timer === 0 && this.state.mode === 'session') {
-      this.setState({ timer: this.state.brkLength * 60, mode: 'break' })
-      this.audio.play()
+      this.setState({ timer: this.state.brkLength * 60, mode: 'break' });
+      this.audio.play();
     }
     if(this.state.timer === 0 && this.state.mode === 'break') {
-      this.setState({ time: this.state.seshLength * 60, mode: 'session' })
-      this.audio.play()  
+      this.setState({ time: this.state.seshLength * 60, mode: 'session' });
+      this.audio.play();
     } 
   }
-  setSeshLength(e) {
-    this.lengthControl('seshLength', e.currentTarget.value, 
-    this.state.seshLength, 'Break');
+
+  // Play or Pause and Start Timer
+  timerControl = () => {
+      if(this.state.active) {
+          this.setState({active: false}, () => clearInterval(this.pomodoro));
+      } else {
+          this.setState({active: true}, () => this.pomodoro = setInterval(() => this.setState({timer: this.state.timer - 1})));
+      }
   }
-  lengthControl(stateToChange, sign, currentLength, timerType) {
-    if (this.state.timerState === 'running') return;
-    if (this.state.timerType === timerType) {
-      if (sign === "-" && currentLength !== 1 ) {
-        this.setState({[stateToChange]: currentLength - 1});
-      } else if (sign === "+" && currentLength !== 60) {
-        this.setState({[stateToChange]: currentLength + 1});
-      } 
-    } else {
-      if (sign === "-" && currentLength !== 1 ) {
-        this.setState({[stateToChange]: currentLength - 1,
-        timer: currentLength * 60 - 60});
-      } else if (sign === "+" && currentLength !== 60) {
-        this.setState({[stateToChange]: currentLength + 1,
-        timer: currentLength * 60 + 60});
-      } 
-    }
-  }
-  timerControl() {
-      // eslint-disable-next-line
-    let control = this.state.timerState === 'stopped' ? (
-      this.beginCountDown(),
-      this.setState({timerState: 'running'})
-    ) : (
-      this.setState({timerState: 'stopped'}),
-      this.state.intervalID && this.state.intervalID.cancel()
-    );
+ 
+  // Increase or Decrease a Timer
+  counter = (increase, type) => {
+      this.setState({[type]: this.state[type] + (increase ? 1 : -1)});
   }
 
-    beginCountDown() {
-            this.setState({
-            intervalID: accurateInterval(() => {
-            this.decrementTimer(); 
-            this.phaseControl();
-        }, 1000)});
-        }
-
-        decrementTimer() {
-            this.setState({timer: this.state.timer - 1});
-        }
-
-        phaseControl() {
-            let timer = this.state.timer;
-            this.warning(timer);
-            this.buzzer(timer);
-            if (timer < 0) { 
-            
-            // eslint-disable-next-line    
-            let phaseC = this.state.timerType === 'Session' ? (this.state.intervalID && this.state.intervalID.cancel(),
-        this.beginCountDown(),
-        this.switchTimer(this.state.brkLength * 60, 'Break')) : 
-      (
-        this.state.intervalID && this.state.intervalID.cancel(),
-        this.beginCountDown(),
-        this.switchTimer(this.state.seshLength * 60, 'Session')
-      );
-                
-            }
-        }
-
-        warning(_timer) {
-            // eslint-disable-next-line
-            let warn = _timer < 61 ? 
-            this.setState({alarmColor: {color: 'orangered'}}) : 
-            this.setState({alarmColor: {color: 'white'}});
-        }
-
-        buzzer(_timer) {
-            if (_timer === 0) {
-                this.audioBeep.play();
-            }
-        }
-
-        switchTimer(num, str) {
-            this.setState({
-            timer: num,
-            timerType: str,
-            alarmColor: {color: 'white'}
-            });
-        }
-
-         clockify() {
-            let minutes = Math.floor(this.state.timer / 60);
-            let seconds = this.state.timer - minutes * 60;
-            seconds = seconds < 10 ? '0' + seconds : seconds;
-            minutes = minutes < 10 ? '0' + minutes : minutes;
-            return minutes + ':' + seconds;
-        }
-
+    
+         
+// Resets Timer
         reset() {
             this.setState({
+            mode: 'session',
+            active: false,
             brkLength: 5,
             seshLength: 25,
-            timerState: 'stopped',
-            timerType: 'Session',
             timer: 1500,
-            intervalID: '',
             alarmColor: {color: 'white'}
+            
         });
-        this.state.intervalID && this.state.intervalID.cancel();
+        
+        clearInterval(this.pomodoro);
         this.audioBeep.pause();
         this.audioBeep.currentTime = 0;
         }
@@ -168,7 +94,7 @@ class Timer extends React.Component {
                     addID="break-increment"
                     lengthID="break-length"
                     title="Break Length" 
-                    onClick={this.setBrkLength}
+                    onClick={this.counter}
                     length={this.state.brkLength}/>
 
                     <TimerLengthControl 
@@ -177,16 +103,16 @@ class Timer extends React.Component {
                     addID="session-increment"
                     lengthID="session-length"
                     title="Session Length" 
-                    onClick={this.setSeshLength} 
+                    onClick={this.counter} 
                     length={this.state.seshLength}/>
 
                     <div className="timer" style={this.state.alarmColor}>
                         <div className="timer-wrapper">
                             <div id='timer-label'>
-                                {this.state.timerType}
+                                {mode === 'session' ? 'Session' : 'Break'}
                             </div>
                             <div id='time-left'>
-                                {this.clockify()}
+                                {moment(this.state.timer).format('mm:ss')}
                             </div>
                         </div>
                     </div>
